769309f20f06d0594f029b0b466acc2c5fac543f
Luphy153 <luangamerpablogamer@gmail.com>
2023-02-03 15:54:42 -0300

    commit inicial1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int num;
+int tabuada;
+Console.WriteLine("bote um numero para a tabuada");
+tabuada = int.Parse(Console.ReadLine());
+
+for (num=1;num<11;num++)
+{
+    
+
+    Console.WriteLine("{0}X{1}={2}", tabuada,num,tabuada*num);
+
+}
+
+int[] n = new int[10];
+for (int m = 0; m < n.Length; m++)
+{
+    n[m] = m;
+    Console.WriteLine("o valor de n[{0}] é: {1}", m, n[m]);
+}
+
